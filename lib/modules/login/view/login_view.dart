726d84fb632cc3ac7fd9cdc4146de5014f416e9c
sujinyang00 <sujinyang00@gmail.com>
2023-09-26 17:06:00 +0900

    Feat: 카카오 로그인 기능 완료(1) --토큰 저장 need

--- lib/modules/login/view/login_view.dart
+++ lib/modules/login/view/login_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -52,8 +53,46 @@ class LoginScreen extends GetView<LoginController> {
 
   @override kakaoLogin() {
     return GestureDetector(
-      onTap: () {
-        Get.toNamed(Routes.home);
+      onTap: () async {
+        try {
+          if (await isKakaoTalkInstalled()) {
+            try {
+              await UserApi.instance.loginWithKakaoTalk();
+              print('카카오톡으로 로그인 성공');
+              Get.toNamed(Routes.main);
+            } catch (error) {
+              print('카카오톡으로 로그인 실패 $error');
+
+              // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+              // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+              if (error is PlatformException && error.code == 'CANCELED') {
+                return;
+              }
+              // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
+              try {
+                await UserApi.instance.loginWithKakaoAccount();
+                print('카카오계정으로 로그인 성공');
+                Get.toNamed(Routes.main);
+              } catch (error) {
+                print('카카오계정으로 로그인 실패 $error');
+              }
+            }
+          } else {
+            try {
+              await UserApi.instance.loginWithKakaoAccount();
+              print('카카오계정으로 로그인 성공');
+            } catch (error) {
+              print('카카오계정으로 로그인 실패 $error');
+            }
+          }
+        } catch (error) {
+          print('Kakao 로그인 실패: $error');
+          if (error is PlatformException && error.code == 'CANCELED') {
+            // 사용자가 KakaoTalk 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+            // 의도적인 로그인 취소로 간주하고 Kakao 계정 로그인을 시도하지 않음 (예: 뒤로 가기)
+            return;
+          }
+        }
       },
       child: Container(
         width: 300.w,
