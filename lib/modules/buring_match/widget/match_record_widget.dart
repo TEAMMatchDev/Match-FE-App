201296c33de08ef3e8e9240381bde5e3810b9088
kimwest00 <minseohufs@hufs.ac.kr>
2023-09-30 19:09:31 +0900

    fix:#15 fix layout detail

--- lib/modules/buring_match/widget/match_record_widget.dart
+++ lib/modules/buring_match/widget/match_record_widget.dart
@@ -7,64 +7,72 @@ import '../../../util/const/style/global_text_styles.dart';
 import '../../home/widget/home_widget.dart';
 
 class MatchRecord extends StatelessWidget {
-  final bool isFinal;
   final String title;
   final String date;
   final List<String> imgList;
+  final bool isChange;
   const MatchRecord(
       {super.key,
-      this.isFinal = false,
       required this.title,
       required this.date,
-      required this.imgList});
+      required this.imgList,
+      this.isChange = false});
 
   @override
   Widget build(BuildContext context) {
     return TimelineTile(
-        alignment: TimelineAlign.start,
-        lineXY: 0.1,
-        isFirst: true,
-        endChild: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  title,
-                  style: AppTextStyles.subtitle3Bold13,
-                ),
-                Text(
-                  date,
-                  style: AppTextStyles.body3Bold12.copyWith(
-                      fontWeight: FontWeight.w500, color: AppColors.grey6),
-                ),
-                Wrap(
-                    spacing: 10.w,
-                    children: imgList
-                        .map((e) => Container(
-                            height: 60.h,
-                            width: 60.w,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10.r),
-                              image: const DecorationImage(
-                                fit: BoxFit.cover,
-                                image: NetworkImage(tmpBackgroundImg),
-                              ),
-                            )))
-                        .toList())
-              ]),
+      alignment: TimelineAlign.start,
+      lineXY: 0.0,
+      isFirst: true,
+      indicatorStyle: IndicatorStyle(
+        indicator: Container(
+          margin: EdgeInsets.symmetric(vertical: 5.h),
+          height: isChange ? 8.h : 14.h,
+          width: isChange ? 8.h : 14.h,
+          decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: isChange ? AppColors.grey5 : AppColors.primary500),
         ),
-        indicatorStyle: IndicatorStyle(
-            width: 14.w,
-            height: 14.h,
-            indicatorXY: 0.1,
-            indicator: Container(
-              height: 14.h,
-              width: 14.h,
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle, color: AppColors.primary500),
-            )));
-    ;
+      ),
+      beforeLineStyle: LineStyle(color: AppColors.grey1, thickness: 2.w),
+      // endChild: Padding(
+      // padding: EdgeInsets.only(left: 8.w, bottom: 30.h),
+      endChild: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              title,
+              style: AppTextStyles.subtitle3Bold13,
+            ),
+            SizedBox(
+              height: 6.h,
+            ),
+            Text(
+              date,
+              style: AppTextStyles.body3Bold12.copyWith(
+                  fontWeight: FontWeight.w500, color: AppColors.grey6),
+            ),
+            SizedBox(
+              height: 10.h,
+            ),
+            Wrap(
+                direction: Axis.horizontal,
+                spacing: 10.w,
+                children: imgList
+                    .map((e) => Container(
+                        height: 60.h,
+                        width: 60.w,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.r),
+                          image: const DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(tmpBackgroundImg),
+                          ),
+                        )))
+                    .toList())
+          ]),
+      // ),
+    );
   }
 }
