ff2587d35ee4a1bbc102485dab4cb6585e1f07d5
kimwest00 <minseohufs@hufs.ac.kr>
2023-10-05 15:58:06 +0900

    feat:#21 카카오 공유하기 api example code 구현

--- lib/util/components/global_bottomsheet.dart
+++ lib/util/components/global_bottomsheet.dart
@@ -1,14 +1,27 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:kakao_flutter_sdk_share/kakao_flutter_sdk_share.dart';
+import 'package:logger/logger.dart';
 import 'package:match/util/const/global_variable.dart';
 import 'package:match/util/const/style/global_text_styles.dart';
-
+import 'package:url_launcher/url_launcher_string.dart';
 import '../const/style/global_color.dart';
 
 class ShareBottomSheet extends StatelessWidget {
-  const ShareBottomSheet({super.key});
+  final String imgUrl;
+  final String usages;
+  final String title;
+  final String appLink;
+  const ShareBottomSheet(
+      {super.key,
+      required this.imgUrl,
+      required this.usages,
+      required this.title,
+      required this.appLink});
 
   @override
   Widget build(BuildContext context) {
@@ -38,9 +51,9 @@ class ShareBottomSheet extends StatelessWidget {
             ),
             //TODO: 후원 상세 페이지에서는 매치 사진 저장하기 부분 삭제
             //TODO: 각 button 로직 추가
-            buttonTile(icon: "save_18", text: "매치 사진 저장하기"),
-            buttonTile(icon: "copy_18", text: "링크 복사하기"),
-            buttonTile(icon: "share_18", text: "카카오톡 공유하기"),
+            buttonTile(icon: "save_18", text: "매치 사진 저장하기", type: "SAVE"),
+            buttonTile(icon: "copy_18", text: "링크 복사하기", type: "LINK"),
+            buttonTile(icon: "share_18", text: "카카오톡 공유하기", type: "KAKAO"),
           ],
         ),
       ),
@@ -48,9 +61,18 @@ class ShareBottomSheet extends StatelessWidget {
   }
 
   /// bottomSheet button tile
-  Widget buttonTile({required String icon, required String text}) {
+  Widget buttonTile(
+      {required String icon, required String text, required type}) {
     return GestureDetector(
-      onTap: () {},
+      onTap: () {
+        switch (type) {
+          case "KAKAO":
+            kakoShare(
+                imgUrl: imgUrl, usages: usages, title: title, appLink: appLink);
+            break;
+          case "LINK":
+        }
+      },
       child: Padding(
         padding: EdgeInsets.symmetric(vertical: 16.h),
         child: Row(
@@ -68,5 +90,54 @@ class ShareBottomSheet extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  ///*카카오톡 공유하기 메소드
+  Future<void> kakoShare(
+      {required String imgUrl,
+      required String usages,
+      required String title,
+      required String appLink}) async {
+    ShareClient shareClient = ShareClient.instance;
+    //카카오톡 설치 여부 판별
+    bool isKakaoTalkSharingAvailable =
+        await shareClient.isKakaoTalkSharingAvailable();
+    //카카오톡 공유하기 위젯
+    final FeedTemplate shareFeed = FeedTemplate(
+      content: Content(
+        title: usages,
+        description: title,
+        imageUrl: Uri.parse(imgUrl),
+        link: Link(
+          mobileWebUrl: Uri.parse(appLink),
+          // androidExecutionParams: ,
+          // iosExecutionParams: ,
+        ),
+      ),
+      buttonTitle: "매치 보러가기",
+    );
+
+    if (isKakaoTalkSharingAvailable) {
+      try {
+        Uri uri = await shareClient.shareDefault(template: shareFeed);
+        await shareClient.launchKakaoTalk(uri);
+        Logger().d('카카오톡 공유 완료');
+      } catch (error) {
+        Logger().d('카카오톡 공유 실패 $error');
+      }
+    } else {
+      try {
+        Uri shareUrl =
+            await WebSharerClient.instance.makeDefaultUrl(template: shareFeed);
+        await launchBrowserTab(shareUrl, popupOpen: true);
+      } catch (error) {
+        print('카카오톡 공유 실패 $error');
+      }
+    }
+  }
+
+  ///*앱 내 딥링크 이동
+  Future<void> launchDeepLink({required String appLink}) async {
+    //TODO:해당 앱 link 복사
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
 import flutter_localization
 import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterLocalizationPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalizationPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
