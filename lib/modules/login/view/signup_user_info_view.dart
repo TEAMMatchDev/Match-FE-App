f6f6ed5e9bc1f6175576af66e3561dd3ddda7109
양수진 <yangsujin@yangsujin-ui-MacBookAir.local>
2023-10-17 16:35:02 +0900

    Fix: 회원가입2 화면 레이아웃 완료

--- lib/modules/login/view/signup_user_info_view.dart
+++ lib/modules/login/view/signup_user_info_view.dart
@@ -34,88 +34,136 @@ class SignUpInfoScreen extends GetView<LoginController> {
     return  Scaffold(
       appBar: CommonAppBar.basic("회원가입"),
       body: Column(
-        children: [
-          SizedBox(height: 35.h),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.w),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  '후원에 필요한\n정보를 입력해주세요.',
-                  style: AppTextStyles.T1Bold18,
-                ),
-                SizedBox(height: 30.h),
-                Text(
-                  '이름',
-                  style: AppTextStyles.T1Bold14,
-                ),
-                SizedBox(height: 10.h),
-                CommonTextField(
-                  textController: controller.idTextController.value,
-                  isSearchScreen: false, //뒤로가기
-                  hasPrefix: false, //검색 아이콘
-                  placeHolder: "이름을 입력해주세요.",
-                  textStatus: controller.searchStatus,
-                  suffixOnTap: () async {},
-                  onSubmitted: (value) async {},
-                  onChanged: ((value) async {
-                    controller.searchStatus.value = SEARCH_STATUS.EDIT;
-                  }),
-                  isPlain: true,
-                ),
-                SizedBox(height: 20.h),
-                Text(
-                  '성별',
-                  style: AppTextStyles.T1Bold14,
-                ),
-                SizedBox(height: 10.h),
-                SelectSexRadioButtons(),
-                SizedBox(height: 10.h),
-                Text(
-                  '생년월일',
-                  style: AppTextStyles.T1Bold14,
-                ),
-                SizedBox(height: 10.h),
-
-                SizedBox(
-                  width: double.infinity,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: AppColors.grey1),
-                      borderRadius: BorderRadius.circular(10.0),
+        children: <Widget>[
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(height: 35.h),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.w),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '후원에 필요한\n정보를 입력해주세요.',
+                          style: AppTextStyles.T1Bold18,
+                        ),
+                        SizedBox(height: 30.h),
+                        Text(
+                          '이름',
+                          style: AppTextStyles.T1Bold14,
+                        ),
+                        SizedBox(height: 10.h),
+                        CommonTextField(
+                          textController: controller.idTextController.value,
+                          isSearchScreen: false, //뒤로가기
+                          hasPrefix: false, //검색 아이콘
+                          placeHolder: "이름을 입력해주세요.",
+                          textStatus: controller.searchStatus,
+                          suffixOnTap: () async {},
+                          onSubmitted: (value) async {},
+                          onChanged: ((value) async {
+                            controller.searchStatus.value = SEARCH_STATUS.EDIT;
+                          }),
+                          isPlain: true,
+                        ),
+                        SizedBox(height: 20.h),
+                        Text(
+                          '성별',
+                          style: AppTextStyles.T1Bold14,
+                        ),
+                        SizedBox(height: 10.h),
+                        SelectSexRadioButtons(),
+                        SizedBox(height: 20.h),
+                        Text(
+                          '생년월일',
+                          style: AppTextStyles.T1Bold14,
+                        ),
+                        SizedBox(height: 10.h),
+                        SizedBox(
+                          width: double.infinity,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(color: AppColors.grey1),
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            child: CallBottomSheet(),
+                          ),
+                        ),
+                        SizedBox(height: 20.h),
+                        Text(
+                          '휴대폰 번호',
+                          style: AppTextStyles.T1Bold14,
+                        ),
+                        SizedBox(height: 10.h),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: CommonTextField(
+                                textController: controller.idTextController.value,
+                                isSearchScreen: false, //뒤로가기
+                                hasPrefix: false, //검색 아이콘
+                                placeHolder: "- 없이 번호만 입력해주세요.",
+                                textStatus: controller.searchStatus,
+                                suffixOnTap: () async {},
+                                onSubmitted: (value) async {},
+                                onChanged: ((value) async {
+                                  controller.searchStatus.value = SEARCH_STATUS.EDIT;
+                                }),
+                                isPlain: true,
+                              ),
+                            ),
+                            SizedBox(width: 10.w),
+                            certinumButton(),
+                          ],
+                        ),
+                        SizedBox(height: 20.h),
+                        Text(
+                          '휴대폰 번호 인증',
+                          style: AppTextStyles.T1Bold14,
+                        ),
+                        SizedBox(height: 10.h),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: CommonTextField(
+                                textController: controller.idTextController.value,
+                                isSearchScreen: false, //뒤로가기
+                                hasPrefix: false, //검색 아이콘
+                                placeHolder: "인증번호를 입력해주세요.",
+                                textStatus: controller.searchStatus,
+                                suffixOnTap: () async {},
+                                onSubmitted: (value) async {},
+                                onChanged: ((value) async {
+                                  controller.searchStatus.value = SEARCH_STATUS.EDIT;
+                                }),
+                                isPlain: true,
+                              ),
+                            ),
+                            SizedBox(width: 10.w),
+                            certinumButton(),
+                          ],
+                        ),
+                        SizedBox(height: 27.h),
+                      ],
                     ),
-                    child: CallBottomSheet(),
-                  ),
-                ),
-
-                SizedBox(height: 27.h),
-                Text(
-                  '비밀번호 확인',
-                  style: AppTextStyles.T1Bold14,
-                ),
-                SizedBox(height: 10.h),
-                CommonTextField(
-                  textController: controller.pwConfirmTextController.value,
-                  isSearchScreen: false, //뒤로가기
-                  hasPrefix: false, //검색 아이콘
-                  placeHolder: "비밀번호를 입력해주세요.",
-                  textStatus: controller.searchStatus,
-                  suffixOnTap: () async {},
-                  onSubmitted: (value) async {},
-                  onChanged: (value) async {},
-                  isPlain: true,
-                ),
-                SizedBox(height: 50.h),
-                CommonButton.login(
-                  text: "확인",
-                  onTap: () async {
-                    Get.back();
-                  },
-                ),
-              ],
+                  )
+                ],
+              ),
             ),
-          )
+          ),
+          SizedBox(height: 8.h),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.w),
+              child: CommonButton.login(
+                      text: "확인",
+                      onTap: () async {
+                        Get.back();
+                      },
+                    ),
+            ),
+          SizedBox(height: 24.h),
         ],
       ),
     );
