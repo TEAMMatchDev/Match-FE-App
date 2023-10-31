6e524b941920067945c45563d5f42d3986677c3c
kimwest00 <minseohufs@hufs.ac.kr>
2023-10-31 15:31:43 +0900

    feat: add web view + 임시 라우팅 해제

--- lib/modules/mypage/view/policy_view.dart
+++ lib/modules/mypage/view/policy_view.dart
@@ -15,17 +15,21 @@ class PolicyScreen extends StatelessWidget {
     return Scaffold(
       appBar: CommonAppBar.basic("운영정책 및 약관"),
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20.w).copyWith(
-          top: 34.h
-        ),
+        padding: EdgeInsets.symmetric(horizontal: 20.w).copyWith(top: 34.h),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             GestureDetector(
-              onTap: (){
-                Get.to(PolicyDetailView(webUrl: "/terms/service",title: "개인정보 처리 방침",));
-              },
-                child: Text("이용약관",style: AppTextStyles.T1Bold15,)),
+                onTap: () {
+                  Get.to(PolicyDetailView(
+                    webUrl: "/terms/service",
+                    title: "개인정보 처리 방침",
+                  ));
+                },
+                child: Text(
+                  "이용약관",
+                  style: AppTextStyles.T1Bold15,
+                )),
             Padding(
               padding: EdgeInsets.symmetric(vertical: 20.h),
               child: Divider(
@@ -35,10 +39,13 @@ class PolicyScreen extends StatelessWidget {
               ),
             ),
             GestureDetector(
-              onTap: (){
-                Get.to(PolicyDetailView(webUrl: "/terms/policy",title: "개인정보 처리 방침",));
-              },
-                child: Text("개인정보 처리 방침",style: AppTextStyles.T1Bold15)),
+                onTap: () {
+                  Get.to(PolicyDetailView(
+                    webUrl: "/terms/policy",
+                    title: "개인정보 처리 방침",
+                  ));
+                },
+                child: Text("개인정보 처리 방침", style: AppTextStyles.T1Bold15)),
             Padding(
               padding: EdgeInsets.symmetric(vertical: 20.h),
               child: Divider(
@@ -48,10 +55,14 @@ class PolicyScreen extends StatelessWidget {
               ),
             ),
             GestureDetector(
-              onTap: (){
-
-              },
-                child: Text("마케팅 정보 수집 및 수신 동의",style: AppTextStyles.T1Bold15)),
+                onTap: () {
+                  Get.to(PolicyDetailView(
+                    webUrl: "/terms/marketing ",
+                    title: "마케팅 정보 수집 및 수신 동의",
+                  ));
+                },
+                child:
+                    Text("마케팅 정보 수집 및 수신 동의", style: AppTextStyles.T1Bold15)),
           ],
         ),
       ),
