121aa1d29b283200098ae70c976106420a2a87ec
kimwest00 <minseohufs@hufs.ac.kr>
2023-11-09 00:42:50 +0900

    fix: 배치 높이관련 문제 해결+ detail 수정

--- lib/modules/tutorial/view/tutorial_view.dart
+++ lib/modules/tutorial/view/tutorial_view.dart
@@ -23,56 +23,81 @@ class TutorialScreen extends GetView<TutorialController> {
           children: [
             Expanded(
               child: Center(
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        categoryType(
-                          type: ProjectType.ELDER,
-                          isSelect: (controller.selectTypeIdx.value == 0).obs,
-                        ),
-                        categoryType(
-                          type: ProjectType.ANIMAL,
-                          isSelect: (controller.selectTypeIdx.value == 1).obs,
-                        ),
-                        categoryType(
-                          type: ProjectType.CHILDREN,
-                          isSelect: (controller.selectTypeIdx.value == 2).obs,
-                        ),
-                      ],
-                    ),
-                    controller.selectTypeIdx.value == -1
-                        ? Padding(
-                            padding: EdgeInsets.only(top: 60.h),
-                            child: Text(
-                                "${AuthService.to.myProfile.value.name}님, 첫 기부금 1원을 어디에 기부할까요?"),
-                          )
-                        : Padding(
-                            padding: EdgeInsets.only(top: 20.h),
-                            child: Stack(
-                              children: [
-                                SvgPicture.asset(
-                                    "${iconDir}ic_speech_${controller.selectTypeIdx.value}_248.svg"),
-                                Positioned(
-                                  top: 10.h,
-                                  child: Text(
-                                    projectTypDescription[ProjectType.ELDER] ??
-                                        "",
-                                    style: AppTextStyles.L1Medium13,
-                                  ),
-                                )
-                              ],
+                child: IntrinsicHeight(
+                  child: Column(
+                    children: [
+                      IntrinsicHeight(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            //TODO: add click event
+                            categoryType(
+                              type: ProjectType.ELDER,
+                              isSelect:
+                                  (controller.selectTypeIdx.value == 0).obs,
                             ),
-                          )
-                  ],
+                            SizedBox(
+                              width: 36.w,
+                            ),
+                            categoryType(
+                              type: ProjectType.ANIMAL,
+                              isSelect:
+                                  (controller.selectTypeIdx.value == 1).obs,
+                            ),
+                            SizedBox(
+                              width: 36.w,
+                            ),
+                            categoryType(
+                              type: ProjectType.CHILDREN,
+                              isSelect:
+                                  (controller.selectTypeIdx.value == 2).obs,
+                            ),
+                          ],
+                        ),
+                      ),
+                      controller.selectTypeIdx.value == -1
+                          ? Padding(
+                              padding: EdgeInsets.only(top: 60.h),
+                              child: Text(
+                                "${AuthService.to.myProfile.value.name}님, 첫 기부금 1원을 \n어디에 기부할까요?",
+                                style: AppTextStyles.S1SemiBold14.copyWith(
+                                    fontSize: 20.sp),
+                                textAlign: TextAlign.center,
+                              ),
+                            )
+                          : Padding(
+                              padding: EdgeInsets.only(top: 20.h),
+                              child: Stack(
+                                children: [
+                                  SvgPicture.asset(
+                                      "${iconDir}ic_speech_${controller.selectTypeIdx.value}_248.svg"),
+                                  Positioned(
+                                    top: 10.h,
+                                    child: Text(
+                                      projectTypDescription[
+                                              ProjectType.ELDER] ??
+                                          "",
+                                      style: AppTextStyles.L1Medium13,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            )
+                    ],
+                  ),
                 ),
               ),
             ),
             CommonButton.edit(
+              isActive: controller.selectTypeIdx.value != -1,
                 text: "기부 분야 선택 완료",
                 onTap: () async {
                   //TODO: api 연결여부 확인 필요
-                })
+                }),
+            SizedBox(
+              height: 24.h,
+            )
           ],
         ),
       ),
