3e1526bb19e9c89037bd44886768651e910b7b3f
kimwest00 <minseohufs@hufs.ac.kr>
2023-11-09 14:24:25 +0900

    feat:#67 fix 말풍선 rendering 에러 + 클릭이벤트 추가

--- lib/modules/tutorial/controller/tutorial_controller.dart
+++ lib/modules/tutorial/controller/tutorial_controller.dart
@@ -1,5 +1,12 @@
 import 'package:get/get.dart';
 
+import '../../../model/enum/project_type.dart';
+
 class TutorialController extends GetxController {
   RxInt selectTypeIdx = (-1).obs;
+  List<ProjectType> projectTypes = [
+    ProjectType.EARTH,
+    ProjectType.ANIMAL,
+    ProjectType.CHILDREN
+  ];
 }
--- lib/modules/tutorial/view/tutorial_view.dart
+++ lib/modules/tutorial/view/tutorial_view.dart
@@ -5,6 +5,7 @@ import 'package:get/get.dart';
 import 'package:match/model/enum/project_type.dart';
 import 'package:match/util/const/style/global_text_styles.dart';
 
+import '../../../provider/routes/routes.dart';
 import '../../../provider/service/auth_service.dart';
 import '../../../util/components/global_button.dart';
 import '../../../util/const/global_variable.dart';
@@ -19,86 +20,108 @@ class TutorialScreen extends GetView<TutorialController> {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 32.w),
-        child: Column(
-          children: [
-            Expanded(
-              child: Center(
-                child: IntrinsicHeight(
-                  child: Column(
-                    children: [
-                      IntrinsicHeight(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            //TODO: add click event
-                            categoryType(
-                              type: ProjectType.ELDER,
-                              isSelect:
-                                  (controller.selectTypeIdx.value == 0).obs,
-                            ),
-                            SizedBox(
-                              width: 36.w,
-                            ),
-                            categoryType(
-                              type: ProjectType.ANIMAL,
-                              isSelect:
-                                  (controller.selectTypeIdx.value == 1).obs,
-                            ),
-                            SizedBox(
-                              width: 36.w,
-                            ),
-                            categoryType(
-                              type: ProjectType.CHILDREN,
-                              isSelect:
-                                  (controller.selectTypeIdx.value == 2).obs,
-                            ),
-                          ],
-                        ),
-                      ),
-                      controller.selectTypeIdx.value == -1
-                          ? Padding(
-                              padding: EdgeInsets.only(top: 60.h),
-                              child: Text(
-                                "${AuthService.to.myProfile.value.name}님, 첫 기부금 1원을 \n어디에 기부할까요?",
-                                style: AppTextStyles.S1SemiBold14.copyWith(
-                                    fontSize: 20.sp),
-                                textAlign: TextAlign.center,
-                              ),
-                            )
-                          : Padding(
-                              padding: EdgeInsets.only(top: 20.h),
-                              child: Stack(
-                                children: [
-                                  SvgPicture.asset(
-                                      "${iconDir}ic_speech_${controller.selectTypeIdx.value}_248.svg"),
-                                  Positioned(
-                                    top: 10.h,
-                                    child: Text(
-                                      projectTypDescription[
-                                              ProjectType.ELDER] ??
-                                          "",
-                                      style: AppTextStyles.L1Medium13,
+        child: Obx(
+          () => Column(
+            children: [
+              Expanded(
+                child: Center(
+                  child: IntrinsicHeight(
+                    child: Column(
+                      children: [
+                        IntrinsicHeight(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              for (var i = 0;
+                                  i < controller.projectTypes.length;
+                                  i++)
+                                GestureDetector(
+                                  onTap: () {
+                                    controller.selectTypeIdx.value = i;
+                                  },
+                                  child: Padding(
+                                    padding: EdgeInsets.only(
+                                        right: i !=
+                                                controller.projectTypes.length -
+                                                    1
+                                            ? 36.w
+                                            : 0),
+                                    child: categoryType(
+                                      type: controller.projectTypes[i],
+                                      isSelect:
+                                          (controller.selectTypeIdx.value == i)
+                                              .obs,
                                     ),
-                                  )
-                                ],
-                              ),
-                            )
-                    ],
+                                  ),
+                                ),
+                            ],
+                          ),
+                        ),
+                        controller.selectTypeIdx.value == -1
+                            ? Padding(
+                                padding: EdgeInsets.only(top: 60.h),
+                                child: Text(
+                                  "${AuthService.to.name.value}님, 첫 기부금 1원을 \n어디에 기부할까요?",
+                                  style: AppTextStyles.S1SemiBold14.copyWith(
+                                      fontSize: 20.sp),
+                                  textAlign: TextAlign.center,
+                                ),
+                              )
+                            : Padding(
+                                padding: EdgeInsets.only(top: 20.h),
+                                child: Container(
+                                    width: 260.w,
+                                    height: 45.h,
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                        fit: BoxFit.fill,
+                                        image: AssetImage(
+                                            "${imgDir}ic_speech_${controller.selectTypeIdx.value}_248.png"),
+                                      ),
+                                    ),
+                                    alignment: Alignment.center,
+                                    child: Stack(
+                                      alignment: Alignment.center,
+                                      children: [
+                                        Positioned(
+                                          top: 13.h,
+                                          child: SizedBox(
+                                            width: 250.w,
+                                            height: 30.h,
+                                            child: Text(
+                                              projectTypDescription[
+                                                      controller.projectTypes[
+                                                          controller
+                                                              .selectTypeIdx
+                                                              .value]] ??
+                                                  "",
+                                              style: AppTextStyles.L1Medium13,
+                                              textAlign: TextAlign.center,
+                                              overflow: TextOverflow.ellipsis,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    )),
+                              )
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-            CommonButton.edit(
-              isActive: controller.selectTypeIdx.value != -1,
-                text: "기부 분야 선택 완료",
-                onTap: () async {
-                  //TODO: api 연결여부 확인 필요
-                }),
-            SizedBox(
-              height: 24.h,
-            )
-          ],
+              CommonButton.edit(
+                  isActive: controller.selectTypeIdx.value != -1,
+                  text: "기부 분야 선택 완료",
+                  onTap: () async {
+                    //TODO: api 연결여부 확인 필요
+                    await Get.toNamed(Routes.main);
+                  }),
+              SizedBox(
+                height: 24.h,
+              )
+            ],
+          ),
         ),
       ),
     );
