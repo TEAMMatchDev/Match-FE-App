e22bf0708ddfb074cea967dd4c0a618b0475a22d
kimwest00 <minseohufs@hufs.ac.kr>
2023-11-11 18:07:09 +0900

    feat: #69 add click event alarm Routes enum으로 key 값에 따른 라우팅 설정

--- lib/provider/service/fcm_service.dart
+++ lib/provider/service/fcm_service.dart
@@ -8,24 +8,25 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
 import 'package:match/provider/routes/routes.dart';
 
+import '../../model/enum/alarm_routes.dart';
 import '../../util/const/style/global_logger.dart';
 import '../../util/method/get_storage.dart';
 import '../api/notification_api.dart';
 
 class FcmService extends GetxService {
   static FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
+  FlutterLocalNotificationsPlugin();
 
   @override
   void onInit() async {
     super.onInit();
     AndroidInitializationSettings androidInitializationSettings =
-        const AndroidInitializationSettings('drawable/splash');
+    const AndroidInitializationSettings('drawable/splash');
     DarwinInitializationSettings iosInitializationSettings =
-        const DarwinInitializationSettings(
-            requestAlertPermission: false,
-            requestBadgePermission: false,
-            requestSoundPermission: false);
+    const DarwinInitializationSettings(
+        requestAlertPermission: false,
+        requestBadgePermission: false,
+        requestSoundPermission: false);
     InitializationSettings initializationSettings = InitializationSettings(
         android: androidInitializationSettings, iOS: iosInitializationSettings);
     await flutterLocalNotificationsPlugin.initialize(initializationSettings);
@@ -34,7 +35,7 @@ class FcmService extends GetxService {
 
   static Future<void> setupInteractedMessage() async {
     RemoteMessage? initialMessage =
-        await FirebaseMessaging.instance.getInitialMessage();
+    await FirebaseMessaging.instance.getInitialMessage();
 
     if (initialMessage != null) {
       _handleMessage(initialMessage);
@@ -44,75 +45,132 @@ class FcmService extends GetxService {
 
   static void _handleMessage(RemoteMessage message) {
     logger.d(message);
-    Get.toNamed(Routes.alarm);
-  }
+    //default 값 선언
+    AlarmRoutes alarmRoutes = AlarmRoutes.ALARM_SCREEN;
+    message.data.forEach((key, value) {
+      logger.d("key: $key, value: $value");
 
-  static Future<void> showNotification(
-      {required String title, required String content}) async {
-    const AndroidNotificationDetails androidNotificationDetails =
-        AndroidNotificationDetails('channel id', 'channel name',
-            channelDescription: 'channel description',
-            importance: Importance.max,
-            priority: Priority.max,
-            showWhen: false);
-    NotificationDetails notificationDetails = const NotificationDetails(
-        android: androidNotificationDetails,
-        iOS: DarwinNotificationDetails(badgeNumber: 1));
-    await flutterLocalNotificationsPlugin.show(
-        0, title, content, notificationDetails);
-  }
+      ///* screen type 변수 업데이트
+      if (key == "screen") {
+        if (alarmScreen[value] != null) {
+          alarmRoutes = alarmScreen[value]!;
+        }
+      }
 
-  Future<String> getDeviceId() async {
-    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-    if (Platform.isAndroid) {
-      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
-      return androidInfo.androidId;
-    } else if (Platform.isIOS) {
-      IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
-      return iosInfo.identifierForVendor;
+      ///* screen arugment에 따라 화면 이동
+      if ((alarmRoutes == AlarmRoutes.MATCH_SCREEN ||
+          alarmRoutes == AlarmRoutes.MATCH_SCREEN) &&
+          key == "projectId") {
+        Get.toNamed(alarmRoutes.routes, arguments: {"projectId": value});
+      } else if (alarmRoutes == AlarmRoutes.HOME_SCREEN) {
+        Get.toNamed(alarmRoutes.routes);
+      } else if (alarmRoutes == AlarmRoutes.EVENT_SCREEN && key == "eventId") {
+        Get.toNamed(alarmRoutes.routes, arguments: {"eventId": value});
+      } else {
+        Get.toNamed(Routes.alarm);
+      }
     }
-    return '';
-  }
 
-  Future<String?> initFirebaseMsg() async {
-    // FirebaseMessaging 인스턴스 초기화
-    FirebaseMessaging messaging = FirebaseMessaging.instance;
-
-    // 토큰 얻기
-    String? token = await messaging.getToken();
-    logger.d('FCM 토큰: $token');
-    return token;
-  }
-
-  ///* fcm(알람) 관련 권한, 기기 등록 api, listener 등록
-  Future<void> setAlarm() async {
-    var deviceId = await GetStorageUtil.getToken(StorageKey.DEVICE_ID);
-
-    ///* alarm 관련 토큰 및 api
-    if (await GetStorageUtil.getToken(StorageKey.FCM_TOKEN) != null &&
-        deviceId != null) {
-    } else {
-      var fcmToken = await initFirebaseMsg();
-      GetStorageUtil.addToken(StorageKey.FCM_TOKEN, fcmToken ?? "");
-      deviceId = await getDeviceId();
-      GetStorageUtil.addToken(StorageKey.DEVICE_ID, deviceId);
-      var tmpResult = await NotificationApi.setAlarmToken(
-          fcmToken: fcmToken!, deviceId: deviceId);
-      logger.d(tmpResult);
+        static Future<void> showNotification(
+        {required String title, required String content})
+    async {
+      const AndroidNotificationDetails androidNotificationDetails =
+      AndroidNotificationDetails('channel id', 'channel name',
+          channelDescription: 'channel description',
+          importance: Importance.max,
+          priority: Priority.max,
+          showWhen: false);
+      NotificationDetails notificationDetails = const NotificationDetails(
+          android: androidNotificationDetails,
+          iOS: DarwinNotificationDetails(badgeNumber: 1));
+      await flutterLocalNotificationsPlugin.show(
+          0, title, content, notificationDetails);
     }
-    await FirebaseMessaging.instance.setAutoInitEnabled(true);
-    FirebaseMessaging.instance.onTokenRefresh.listen((token) async {
-      logger.w("!!!refreseh Token: $token");
-      GetStorageUtil.addToken(StorageKey.FCM_TOKEN, token ?? "");
-      var tmpResult = await NotificationApi.setAlarmToken(
-          fcmToken: token, deviceId: deviceId ?? "");
-    }).onError((err) {
-      logger.e("!!!refreseh Token: $err");
-    });
 
-    ///* alarm listener 등록
-    FirebaseMessaging.onMessage.listen((RemoteMessage? message) {
-      logger.w("!!!onMessage: $message");
+    Future<String> getDeviceId() async {
+      DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+      if (Platform.isAndroid) {
+        AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+        return androidInfo.androidId;
+      } else if (Platform.isIOS) {
+        IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+        return iosInfo.identifierForVendor;
+      }
+      return '';
+    }
+
+    Future<String?> initFirebaseMsg() async {
+      // FirebaseMessaging 인스턴스 초기화
+      FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+      // 토큰 얻기
+      String? token = await messaging.getToken();
+      logger.d('FCM 토큰: $token');
+      return token;
+    }
+
+    ///* fcm(알람) 관련 권한, 기기 등록 api, listener 등록
+    Future<void> setAlarm() async {
+      var deviceId = await GetStorageUtil.getToken(StorageKey.DEVICE_ID);
+
+      ///* alarm 관련 토큰 및 api
+      if (await GetStorageUtil.getToken(StorageKey.FCM_TOKEN) != null &&
+          deviceId != null) {} else {
+        var fcmToken = await initFirebaseMsg();
+        GetStorageUtil.addToken(StorageKey.FCM_TOKEN, fcmToken ?? "");
+        deviceId = await getDeviceId();
+        GetStorageUtil.addToken(StorageKey.DEVICE_ID, deviceId);
+        var tmpResult = await NotificationApi.setAlarmToken(
+            fcmToken: fcmToken!, deviceId: deviceId);
+        logger.d(tmpResult);
+      }
+      await FirebaseMessaging.instance.setAutoInitEnabled(true);
+      FirebaseMessaging.instance.onTokenRefresh.listen((token) async {
+        logger.w("!!!refreseh Token: $token");
+        GetStorageUtil.addToken(StorageKey.FCM_TOKEN, token ?? "");
+        var tmpResult = await NotificationApi.setAlarmToken(
+            fcmToken: token, deviceId: deviceId ?? "");
+      }).onError((err) {
+        logger.e("!!!refreseh Token: $err");
+      });
+
+      ///* alarm listener 등록
+      FirebaseMessaging.onMessage.listen((RemoteMessage? message) {
+        logger.w("!!!onMessage: $message");
+        if (message != null) {
+          if (message.notification != null) {
+            logger.d(message.notification!.title);
+            logger.d(message.notification!.body);
+            FcmService.showNotification(
+                title: message.notification!.title ?? "",
+                content: message.notification!.body ?? "");
+          }
+        }
+      });
+      FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage? message) {
+        logger.w("!!!onMessageOpenedApp: $message");
+        if (message != null) {
+          if (message.notification != null) {
+            logger.d(message.notification!.title);
+            logger.d(message.notification!.body);
+            FcmService.showNotification(
+                title: message.notification!.title ?? "",
+                content: message.notification!.body ?? "");
+          }
+        }
+      });
+      FirebaseMessaging.onBackgroundMessage(
+          _firebaseMessagingBackgroundHandler);
+    }
+
+    @pragma('vm:entry-point')
+    Future<void> _firebaseMessagingBackgroundHandler(
+        RemoteMessage? message) async {
+      // If you're going to use other Firebase services in the background, such as Firestore,
+      // make sure you call `initializeApp` before using other Firebase services.
+      await Firebase.initializeApp();
+
+      logger.w("!!!Handling a background message: ${message?.messageId}");
       if (message != null) {
         if (message.notification != null) {
           logger.d(message.notification!.title);
@@ -122,38 +180,5 @@ class FcmService extends GetxService {
               content: message.notification!.body ?? "");
         }
       }
-    });
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage? message) {
-      logger.w("!!!onMessageOpenedApp: $message");
-      if (message != null) {
-        if (message.notification != null) {
-          logger.d(message.notification!.title);
-          logger.d(message.notification!.body);
-          FcmService.showNotification(
-              title: message.notification!.title ?? "",
-              content: message.notification!.body ?? "");
-        }
-      }
-    });
-    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-  }
-
-  @pragma('vm:entry-point')
-  Future<void> _firebaseMessagingBackgroundHandler(
-      RemoteMessage? message) async {
-    // If you're going to use other Firebase services in the background, such as Firestore,
-    // make sure you call `initializeApp` before using other Firebase services.
-    await Firebase.initializeApp();
-
-    logger.w("!!!Handling a background message: ${message?.messageId}");
-    if (message != null) {
-      if (message.notification != null) {
-        logger.d(message.notification!.title);
-        logger.d(message.notification!.body);
-        FcmService.showNotification(
-            title: message.notification!.title ?? "",
-            content: message.notification!.body ?? "");
-      }
     }
   }
-}
