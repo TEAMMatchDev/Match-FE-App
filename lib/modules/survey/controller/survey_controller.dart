7539aaf89662abd7528f1af0a333eddafce4efc8
kimwest00 <minseohufs@hufs.ac.kr>
2023-11-17 17:20:55 +0900

    feat:#17 add textfield maxline, length 설정 + 자동 scroll 설정

--- lib/modules/survey/controller/survey_controller.dart
+++ lib/modules/survey/controller/survey_controller.dart
@@ -1,6 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import '../../../util/const/style/global_logger.dart';
 
 class SurveyController extends GetxController {
   Rx<int> donateRate = 4.obs;
@@ -8,6 +9,39 @@ class SurveyController extends GetxController {
   Rx<int> informRate = 4.obs;
 
   Rx<TextEditingController> commentTextController = TextEditingController().obs;
-
+  Rx<ScrollController> scrollController = ScrollController().obs;
   RxBool isSubmit = false.obs;
+
+  // 키보드 상태에 따라 스크롤 조절
+  void _scrollListener() async{
+    var status =  _isKeyboardVisible();
+    logger.e(status);
+    if (status) {
+      // 키보드가 올라온 경우
+      double offset = scrollController.value.position.maxScrollExtent;
+      logger.e(offset);
+      await scrollController.value.animateTo(offset+100, duration: Duration(milliseconds: 200), curve: Curves.easeInOut);
+      logger.e(scrollController.value.position.pixels);
+
+    } else {
+      // 키보드가 내려간 경우
+      await scrollController.value.animateTo(0, duration: Duration(milliseconds: 200), curve: Curves.easeInOut);
+    }
+  }
+
+  // 키보드가 화면에 보이는지 확인
+  bool _isKeyboardVisible() {
+    final focusNode = FocusScope.of(Get.context!);
+    return !focusNode.hasPrimaryFocus && focusNode.hasFocus;
+  }
+  @override
+  void onInit() {
+    super.onInit();
+    scrollController.value.addListener(_scrollListener);
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    scrollController.value.dispose();
+  }
 }
--- lib/modules/survey/view/survey_view.dart
+++ lib/modules/survey/view/survey_view.dart
@@ -22,93 +22,110 @@ class SurveyScreen extends GetView<SurveyController> {
 
           ///* 입력 내용이 많아졌을때 overFlow 에러를 방지하기위해 listview로 설정
           Obx(
-        () => ListView(
+        () => Stack(
           children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.w),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 24.h,
-                  ),
-                  Text(
-                    "솔직한 리뷰를 남겨주세요",
-                    style: AppTextStyles.T1Bold16,
-                  ),
-                  SizedBox(
-                    height: 10.h,
-                  ),
-                  Center(
-                    child: Column(
-                      children: [
-                        RateWidget(
-                            title: "후원 경험",
-                            subtitle: "정기적으로 후원할 수 있는 환경이 제공되었나요?",
-                            rate: controller.donateRate),
-                        SizedBox(
-                          height: 10.h,
-                        ),
-                        RateWidget(
-                            title: "투명성",
-                            subtitle: "후원 단체에 대한 신뢰가 형성되었나요?",
-                            rate: controller.truthRate),
-                        SizedBox(
-                          height: 10.h,
-                        ),
-                        RateWidget(
-                            title: "정보 제공",
-                            subtitle: "후원 과정에서 충분한 안내를 받았나요?",
-                            rate: controller.informRate),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 10.h,
-            ),
-            GreySizedBox(),
-            SizedBox(
-              height: 10.h,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.w),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "MATCH와 함께 한 후원은 어떠셨나요?",
-                      style: AppTextStyles.T1Bold16,
-                    ),
-                    SizedBox(
-                      height: 5.h,
-                    ),
-                    SizedBox(
-                      height: 100.h,
-                      child: CommonInputField.survey(
-                        textController: controller.commentTextController.value,
-                        onChange: (value) async {
-                          if (value.length >= 1000) {
-                            Fluttertoast.showToast(
-                                msg: "리뷰 내용은 최대 1,000자까지 작성 가능해요.");
-                          }
-                        },
+            ListView(
+              scrollDirection: Axis.vertical,
+              controller: controller.scrollController.value,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.w),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 24.h,
                       ),
-                    ),
-                    SizedBox(
-                      height: 24.h,
-                    ),
-                    CommonButton.edit(
-                      text: "작성 완료",
-                      isActive: controller.isSubmit.value,
-                      onTap: () async {
-                        //TODO: 리뷰 등록 api
-                      },
-                    )
-                  ]),
+                      Text(
+                        "솔직한 리뷰를 남겨주세요",
+                        style: AppTextStyles.T1Bold16,
+                      ),
+                      SizedBox(
+                        height: 10.h,
+                      ),
+                      Center(
+                        child: Column(
+                          children: [
+                            RateWidget(
+                                title: "후원 경험",
+                                subtitle: "정기적으로 후원할 수 있는 환경이 제공되었나요?",
+                                rate: controller.donateRate),
+                            SizedBox(
+                              height: 10.h,
+                            ),
+                            RateWidget(
+                                title: "투명성",
+                                subtitle: "후원 단체에 대한 신뢰가 형성되었나요?",
+                                rate: controller.truthRate),
+                            SizedBox(
+                              height: 10.h,
+                            ),
+                            RateWidget(
+                                title: "정보 제공",
+                                subtitle: "후원 과정에서 충분한 안내를 받았나요?",
+                                rate: controller.informRate),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 10.h,
+                ),
+                GreySizedBox(),
+                SizedBox(
+                  height: 10.h,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.w),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "MATCH와 함께 한 후원은 어떠셨나요?",
+                          style: AppTextStyles.T1Bold16,
+                        ),
+                        SizedBox(
+                          height: 5.h,
+                        ),
+                        SizedBox(
+                          height: 100.h,
+                          child: CommonInputField.survey(
+                            textController:
+                                controller.commentTextController.value,
+                            onChange: (value) async {
+                              if (value.length >= 1000) {
+                                Fluttertoast.showToast(
+                                    msg: "리뷰 내용은 최대 1,000자까지 작성 가능해요.");
+                                // 1000번째 인덱스 이후의 값 삭제
+                                value.substring(0, 1000);
+                              }
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          height: 24.h,
+                        ),
+                      ]),
+                ),
+              ],
             ),
+            Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 29.w, vertical: 20.h),
+                  child: CommonButton.edit(
+                    text: "작성 완료",
+                    isActive: controller.isSubmit.value,
+                    onTap: () async {
+                      //TODO: 리뷰 등록 api
+                    },
+                  ),
+                ))
           ],
         ),
       ),
