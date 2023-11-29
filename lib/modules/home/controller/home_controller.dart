28b46452dd9d5f780e07a631c3b58c9dae66185f
kimwest00 <minseohufs@hufs.ac.kr>
2023-11-29 22:25:27 +0900

    fix: ad부분 기획 변경 사항 적용

--- lib/modules/home/controller/home_controller.dart
+++ lib/modules/home/controller/home_controller.dart
@@ -3,7 +3,9 @@ import 'package:kakao_flutter_sdk/kakao_flutter_sdk_share.dart';
 import 'package:match/provider/api/banner_api.dart';
 import 'package:match/provider/api/flame_api.dart';
 import 'package:match/provider/api/util/global_api_field.dart';
+import 'package:match/util/const/global_variable.dart';
 import '../../../model/banner/banners.dart';
+import '../../../model/enum/banner_type.dart';
 import '../../../model/flame/flame.dart';
 import '../../../util/const/style/global_logger.dart';
 import '../../../util/method/dynamic_link.dart';
@@ -16,8 +18,18 @@ class HomeController extends GetxController {
   RxInt currentIdx = 1.obs;
 
   /// banner 전체 개수
-  RxInt adCount = 0.obs;
-  RxList<Banners> bannerList = <Banners>[].obs;
+  RxInt adCount = 2.obs;
+  RxList<Banners> bannerList = <Banners>[
+    Banners(
+        bannerId: -2,
+        bannerType: BannerType.CONTENTS.name,
+        bannerImg: imgDir + "iv_ad_1.png"),
+    Banners(
+        bannerId: -1,
+        bannerType: BannerType.CONTENTS.name,
+        bannerImg: imgDir + "iv_ad_2.png",
+        contentsUrl: "https://litt.ly/match.official"),
+  ].obs;
 
   /// flame 전체 개수
   /// * flame 전체 개수를 화면에 text형태로 표시하기 위해
@@ -96,8 +108,9 @@ class HomeController extends GetxController {
     /// MainBinding에서 mypage API와 동시 호출되어 refresh api 중복호출 가능성이 있음
     /// 이에 API를 3초 가량 delay후 호출
     Future.delayed(Duration(seconds: 2), () async {
-      bannerList.assignAll(await BannerApi.getBannerList());
-      adCount.value = bannerList.length;
+      //기획 변경으로 인한 api 사용 X
+      // bannerList.assignAll(await BannerApi.getBannerList());
+      // adCount.value = bannerList.length;
 
       flameList.assignAll(await FlameApi.getBurningFlameList());
       totalCnt = FlameApi.burningFlame.totalCnt.obs;
