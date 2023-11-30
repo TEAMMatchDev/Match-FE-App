076b4f86995889cbb4fd1ca75c1662f09a9cd072
kimwest00 <minseohufs@hufs.ac.kr>
2023-11-30 13:37:24 +0900

    fix:#102 튜토리얼 리렌더링 속도 개선

--- lib/modules/signIn/view/mail_login_view.dart
+++ lib/modules/signIn/view/mail_login_view.dart
@@ -38,129 +38,129 @@ class EmailLoginScreen extends GetView<LoginController> {
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     String logintype = 'NOMAL';
-    return  Scaffold(
+    return Scaffold(
       appBar: CommonAppBar.basic("로그인"),
       body: Column(
-          children: <Widget>[
-            Expanded(
-              child: Column(
-                  children: [
-                  SizedBox(height: 35.h),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20.w),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          '이메일',
-                          style: AppTextStyles.T1Bold14,
-                        ),
-                        SizedBox(height: 10.h),
-                        //TODO) api 연동
-                        CommonInputField.signInID(
-                            textController : controller.idTextController.value,
-                            onChange: (value) async {
-                              //print(">>> 입력한 id: $value");
-                              controller.userId.value = value;
-                            }),
-                        SizedBox(height: 20.h),
-                        Text(
-                          '비밀번호',
-                          style: AppTextStyles.T1Bold14,
-                        ),
-                        SizedBox(height: 10.h),
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              children: [
+                SizedBox(height: 35.h),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20.w),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '이메일',
+                        style: AppTextStyles.T1Bold14,
+                      ),
+                      SizedBox(height: 10.h),
+                      //TODO) api 연동
+                      CommonInputField.signInID(
+                          textController: controller.idTextController.value,
+                          onChange: (value) async {
+                            //print(">>> 입력한 id: $value");
+                            controller.userId.value = value;
+                          }),
+                      SizedBox(height: 20.h),
+                      Text(
+                        '비밀번호',
+                        style: AppTextStyles.T1Bold14,
+                      ),
+                      SizedBox(height: 10.h),
                       CommonInputField.signInPW(
-                          textController : controller.pwTextController.value,
+                          textController: controller.pwTextController.value,
                           onChange: (value) async {
                             //print(">>> 입력한 pw: $value");
                             controller.userPw.value = value;
                           }),
-                        SizedBox(height: 27.h),
-                        Center(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              GestureDetector(
-                                onTap: () {
-                                  Get.to(SignUpMailScreen());
-                                },
-                                child: Text(
-                                  "회원가입",
-                                  style: AppTextStyles.T1Bold12.copyWith(color: AppColors.grey6),
-                                ),
+                      SizedBox(height: 27.h),
+                      Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                Get.to(SignUpMailScreen());
+                              },
+                              child: Text(
+                                "회원가입",
+                                style: AppTextStyles.T1Bold12.copyWith(
+                                    color: AppColors.grey6),
                               ),
-                              SizedBox(width: 10.w),
-                              Text(
-                                "|",
-                                style: AppTextStyles.T1Bold12.copyWith(color: AppColors.grey1),
+                            ),
+                            SizedBox(width: 10.w),
+                            Text(
+                              "|",
+                              style: AppTextStyles.T1Bold12.copyWith(
+                                  color: AppColors.grey1),
+                            ),
+                            SizedBox(width: 10.w),
+                            GestureDetector(
+                              onTap: () {
+                                Get.to(FindPwScreen());
+                              },
+                              child: Text(
+                                "비밀번호 찾기",
+                                style: AppTextStyles.T1Bold12.copyWith(
+                                    color: AppColors.grey6),
                               ),
-                              SizedBox(width: 10.w),
-                              GestureDetector(
-                                onTap: () {
-                                  Get.to(FindPwScreen());
-                                },
-                                child: Text(
-                                  "비밀번호 찾기",
-                                  style: AppTextStyles.T1Bold12.copyWith(color: AppColors.grey6),
-                                ),
-                              ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ],),
+                ),
+              ],
             ),
-            SizedBox(height: 8.h),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.w),
-              child: CommonButton.login(
-                text: "로그인",
-                onTap: () async {
-                  if(controller.userId.value != '' && controller.userPw.value != '') {
-                    ///이메일 유효성 검사
-                    bool isValidEmail = isEmail(controller.userId.value);
-                    if (isValidEmail) {
-                      var result = await UserAuthApi.setSignIn(
-                          email: controller.userId.value,
-                          password: controller.userPw.value);
-                      if (result) {
-                        controller.setPlatform('email');
-                        //print(">> 로그인한 플랫폼: ${controller.loginPlatform}");
-                        if(AuthService.to.isTutorial.value) {
-                          await AuthService.to.getUserInfo();
-                          Get.to(()=>const InitTutorialScreen());
-                        }
-                        else {
-                          Get.offAllNamed(Routes.main);
-                        }
-                      }
-                      else {
-                        Fluttertoast.showToast(msg: "로그인에 실패했습니다.");
-                      }
+          ),
+          SizedBox(height: 8.h),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            child: CommonButton.login(
+              text: "로그인",
+              onTap: () async {
+                if (controller.userId.value != '' &&
+                    controller.userPw.value != '') {
+                  ///이메일 유효성 검사
+                  bool isValidEmail = isEmail(controller.userId.value);
+                  if (isValidEmail) {
+                    var result = await UserAuthApi.setSignIn(
+                        email: controller.userId.value,
+                        password: controller.userPw.value);
+                    if (result) {
+                      controller.setPlatform('email');
+                      //print(">> 로그인한 플랫폼: ${controller.loginPlatform}");
+                      // if(AuthService.to.isTutorial.value) {
+                      await AuthService.to.getUserInfo();
+                      Get.to(() => const InitTutorialScreen());
+                      // }
+                      // else {
+                      //   Get.offAllNamed(Routes.main);
+                      // }
+                    } else {
+                      Fluttertoast.showToast(msg: "로그인에 실패했습니다.");
                     }
-                    else {
-                      Fluttertoast.showToast(msg: "이메일 형식을 다시 확인해주세요");
-                    }
+                  } else {
+                    Fluttertoast.showToast(msg: "이메일 형식을 다시 확인해주세요");
                   }
-                  else { //아이디 비밀번호 둘 중 하나 ''ㄴ
-                    Fluttertoast.showToast(msg: "아이디와 비밀번호를 입력해주세요.");
-                  }
-                },
-              ),
+                } else {
+                  //아이디 비밀번호 둘 중 하나 ''ㄴ
+                  Fluttertoast.showToast(msg: "아이디와 비밀번호를 입력해주세요.");
+                }
+              },
             ),
-            SizedBox(height: 24.h),
-          ],
+          ),
+          SizedBox(height: 24.h),
+        ],
       ),
     );
   }
-
 }
-
-
 
 /*child: GestureDetector(
 onTap: () {
--- lib/modules/tutorial/controller/tutorial_controller.dart
+++ lib/modules/tutorial/controller/tutorial_controller.dart
@@ -3,11 +3,12 @@ import 'package:match/model/recommend_project/recommend_project.dart';
 
 import '../../../model/enum/project_type.dart';
 import '../../../provider/api/tutorial_api.dart';
+import '../../../util/const/global_mock_data.dart';
 
 class TutorialController extends GetxController {
-  RxInt selectTypeIdx = (-1).obs;
-  Rx<RecommendProject>? selectProject;
-  List<RecommendProject> projectTypes = [];
+  Rx<int> selectTypeIdx = (-1).obs;
+  Rx<RecommendProject> selectProject = tmpRecommendProject.obs;
+  RxList<RecommendProject> projectTypes = <RecommendProject>[].obs;
 
   @override
   void onInit() async {
